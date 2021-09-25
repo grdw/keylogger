201b0de0b2b84e77fb00fe503fa61f6ddbe31230
grdw <gerard@grdw.nl>
2021-09-25 19:22:52 +0200

    Wrap input devices in a keylogger class

---
+++ keylogger.gemspec
@@ -0,0 +1,32 @@
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require "keyloggeralizer/version"
+
+Gem::Specification.new do |spec|
+  spec.name          = "keylogger"
+  spec.version       = "0.0.1"
+  spec.authors       = ["grdw"]
+  spec.email         = ["gerard@grdw.nl"]
+
+  spec.summary       = "A basic keylogger"
+  spec.description   = "A basic keylogger"
+  spec.homepage      = "https://github.com/grdw/keylogger"
+  spec.license       = "MIT"
+
+  # Specify which files should be added to the gem when it is released.
+  # The `git ls-files -z` loads the files in the RubyGem that have been added
+  # into git.
+  spec.files         = Dir.chdir(File.expand_path(__dir__)) do
+    `git ls-files -z`.split("\x0").reject do |f|
+      f.match(%r{^(test|spec|features)/})
+    end
+  end
+
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", ">= 2.2.10"
+  spec.add_development_dependency "pry", "~> 0.13"
+  spec.add_development_dependency "rake", "~> 13.0"
+  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "rubocop", "~> 0.82"
+end
--- lib/input_devices.rb
+++ lib/input_devices.rb
@@ -1,23 +1,25 @@
-module InputDevices
-  KEYBOARD_EV = "120013".freeze
-  DEVICE_REGEX = /N: Name="(?<name>.+)"(\n|.)*H: Handlers=.*event(?<event_id>[0-9]+).*(\n|.)*B: EV=(?<ev>[0-9]+)/.freeze
+class Keylogger
+  module InputDevices
+    KEYBOARD_EV = "120013".freeze
+    DEVICE_REGEX = /N: Name="(?<name>.+)"(\n|.)*H: Handlers=.*event(?<event_id>[0-9]+).*(\n|.)*B: EV=(?<ev>[0-9]+)/.freeze
 
-  Device = Struct.new(:name, :event_id, :ev, keyword_init: true)
+    Device = Struct.new(:name, :event_id, :ev, keyword_init: true)
 
-  def self.find_by_name(name)
-    devices.detect do |device|
-      device.name == name && device.ev == KEYBOARD_EV
+    def self.find_by_name(name)
+      devices.detect do |device|
+        device.name == name && device.ev == KEYBOARD_EV
+      end
     end
-  end
 
-  private
+    private
 
-  def self.devices
-    @devices ||= begin
-      contents = File.read("/proc/bus/input/devices")
-      contents.split("\n\n").map do |device|
-        blob = DEVICE_REGEX.match(device).named_captures
-        Device.new(blob)
+    def self.devices
+      @devices ||= begin
+        contents = File.read("/proc/bus/input/devices")
+        contents.split("\n\n").map do |device|
+          blob = DEVICE_REGEX.match(device).named_captures
+          Device.new(blob)
+        end
       end
     end
   end
