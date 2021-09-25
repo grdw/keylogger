76d2dd112090e80fee4e51295eb7162ebb5925a4
grdw <gerard@grdw.nl>
2021-09-25 19:31:44 +0200

    use loop do

--- lib/keylogger.rb
+++ lib/keylogger.rb
@@ -10,7 +10,7 @@ class Keylogger
 
     unless SUPPORTED_PLATFORMS.include?(RUBY_PLATFORM)
       raise WrongPlatformError,
-        "platform #{RUBY_PLATFORM} not supported"
+            "platform #{RUBY_PLATFORM} not supported"
     end
 
     raise DeviceNotFoundError unless device
@@ -18,7 +18,8 @@ class Keylogger
 
   def listen
     file = File.open("/dev/input/event" + device.event_id, "rb")
-    while line = file.read(24)
+    loop do
+      line = file.read(24)
       value = line.unpack(FORMAT).last
 
       if value > 0 && value < 59
