67d9489b246bbb06c6bac5fe59f67cddafea54f1
grdw <gerard@grdw.nl>
2021-09-25 19:23:15 +0200

    Fix specs

--- spec/input_devices_spec.rb
+++ spec/input_devices_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
-RSpec.describe InputDevices do
+RSpec.describe Keylogger::InputDevices do
   it "finds a device by a certain name" do
     expect(File).to receive(:read)
       .and_return(File.read("spec/mocked_devices"))
 
-    device = InputDevices.find_by_name("Keychron Keychron K2")
+    device = Keylogger::InputDevices.find_by_name("Keychron Keychron K2")
   end
 end
