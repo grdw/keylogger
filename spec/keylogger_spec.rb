5de4ed54bf76b2c19f35ac589632e131afd506ca
grdw <gerard@grdw.nl>
2021-09-26 16:03:32 +0200

    Refactor tests a bit

--- spec/keylogger_spec.rb
+++ spec/keylogger_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Keylogger do
   describe "#initialize" do
     it "returns a keylogger instance" do
       device = Keylogger::InputDevices::LinuxX86::Device.new(name: "Test", event_id: "12", ev: "1")
-      expect_any_instance_of(Keylogger::InputDevices::LinuxX86).to receive(:find_by_name).and_return(device)
+      expect(Keylogger::InputDevices.device).to receive(:find_by_name).and_return(device)
       keylogger = Keylogger.new("Keychron Keychron K2")
 
       expect(keylogger).to be_a(Keylogger)
@@ -18,8 +18,8 @@ RSpec.describe Keylogger do
 
     it "raises an error when device is not found" do
       device = nil
-      expect_any_instance_of(Keylogger::InputDevices::LinuxX86).to receive(:find_by_name).and_return(device)
-      expect_any_instance_of(Keylogger::InputDevices::LinuxX86).to receive(:default).and_return(device)
+      expect(Keylogger::InputDevices.device).to receive(:find_by_name).and_return(device)
+      expect(Keylogger::InputDevices.device).to receive(:default).and_return(device)
       expect { Keylogger.new("Wrong device") }.to raise_error(Keylogger::DeviceNotFoundError)
     end
   end
